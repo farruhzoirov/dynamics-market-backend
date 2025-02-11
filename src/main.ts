c225dc97f497d625e4124cfa916903a453193c28
farruhzoirov <fzoirov29@gmail.com>
2025-02-11 22:49:30 +0500

    commit

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
+    allowedHeaders: 'Content-Type, Authorization, Language, Accept',
   });
 
   // Swagger based
