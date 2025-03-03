3197f26b7fd6c2a9e3cf0537dcb4ab7b64d82eab
farruhzoirov <fzoirov29@gmail.com>
2025-03-04 00:10:28 +0500

    commit

--- src/modules/brand/brand.module.ts
+++ src/modules/brand/brand.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BrandController } from './brand.controller';
 import { BrandService } from './brand.service';
 import { Brand, BrandSchema } from './schemas/brand.schema';
+import { Product, ProductSchema } from '../product/schemas/product.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Brand.name, schema: BrandSchema }]),
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
   controllers: [BrandController],
   providers: [BrandService],
