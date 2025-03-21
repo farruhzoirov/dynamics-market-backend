e26e43a135e8aed869e0089383030206b90addd1
farruhzoirov <fzoirov29@gmail.com>
2025-03-21 14:58:26 +0500

    commit

--- src/shared/services/redis.service.ts
+++ src/shared/services/redis.service.ts
@@ -4,7 +4,10 @@ import Redis from 'ioredis';
 @Injectable()
 export class RedisService extends Redis {
   constructor() {
-    super();
+    super({
+      host: 'redis',
+      port: 6379,
+    });
     super.on('error', (error) => {
       console.log('Error Connecting to Redis');
       process.exit(1);
