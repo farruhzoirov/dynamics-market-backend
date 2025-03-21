d28c250bf8538c933a4486ac3109589473be4002
farruhzoirov <fzoirov29@gmail.com>
2025-03-21 15:12:38 +0500

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ import jwtConfig from './config/jwt.config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => {
+        console.log(configService.get(CONFIG_DATABASE).users.uri);
         return {
           uri: configService.get(CONFIG_DATABASE).users.uri,
           writeConcern: { w: 'majority' },
