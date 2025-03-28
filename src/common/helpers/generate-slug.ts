1070a2543812fc3c4ba1466450b4368939e23533
farruhzoirov <fzoirov29@gmail.com>
2025-03-28 16:53:29 +0500

    commit

--- src/common/helpers/generate-slug.ts
+++ src/common/helpers/generate-slug.ts
@@ -2,11 +2,11 @@ import slugify from 'slugify';
 import { v4 as uuid4 } from 'uuid';
 
 export const generateUniqueSlug = (name: string) => {
-  return `${slugify(name, { lower: true, replacement: '-', strict: false })}`;
+  return `${slugify(name, { lower: true, replacement: '-', strict: true })}`;
 };
 
 export const generateUniqueSlugForProduct = (name: string) => {
-  const slug = `${slugify(name, { lower: true, replacement: '-', strict: false })}`;
+  const slug = `${slugify(name, { lower: true, replacement: '-', strict: true })}`;
   const uniqueChar = uuid4()
     .replace(/[^a-z]/g, '')
     .charAt(0);
