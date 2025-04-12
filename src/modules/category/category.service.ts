c8d46cf7fa281754a61878efbe2b1b21b2a755c7
farruhzoirov <fzoirov29@gmail.com>
2025-04-12 11:00:50 +0500

    commit

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -33,7 +33,9 @@ export class CategoryService {
   async getCategoriesForFront(body: GetCategoryDto, lang: string) {
     const cacheKey = `categories:${lang}`;
     const cachedData = await this.redisService.getData(cacheKey);
+    console.log('cachedData', cachedData);
     if (cachedData) {
+      console.log('ok');
       return { data: cachedData };
     }
     const pipeline = await buildCategoryHierarchyPipeline(lang);
