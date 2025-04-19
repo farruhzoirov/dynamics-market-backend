6859e94fbfa62ed5bb9c947bffff6700ce154dcb
farruhzoirov <fzoirov29@gmail.com>
2025-04-19 18:39:55 +0500

    commit

--- src/modules/cart/cart.service.ts
+++ src/modules/cart/cart.service.ts
@@ -1,9 +1,9 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Cart, CartDocument } from './schemas/cart.schema';
 import { AddToCartDto } from './dto/cart.dto';
 import { IJwtPayload } from 'src/shared/interfaces/jwt-payload';
+import { Cart, CartDocument } from './schemas/cart.schema';
 import { Product, ProductDocument } from '../product/schemas/product.model';
 
 @Injectable()
