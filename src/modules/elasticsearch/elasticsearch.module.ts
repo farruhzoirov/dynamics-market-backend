d1350032913396bd27be50ffa3b81577427be8ab
farruhzoirov <fzoirov29@gmail.com>
2025-05-19 09:58:26 +0500

    commit

--- src/modules/elasticsearch/elasticsearch.module.ts
+++ src/modules/elasticsearch/elasticsearch.module.ts
@@ -7,7 +7,7 @@ import { ElasticsearchController } from './elasticsearch.controller';
   imports: [
     ElasticsearchModule.registerAsync({
       useFactory: () => ({
-        node: 'http://172.18.0.2:9200',
+        node: 'http://elasticsearch:9200',
       }),
     }),
   ],
