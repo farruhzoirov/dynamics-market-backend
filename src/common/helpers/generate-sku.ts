5085862332a39b5f631db9dca083249e34062965
farruhzoirov <fzoirov29@gmail.com>
2025-07-07 11:01:56 +0500

    Update SKU generation logic to determine the next SKU based on the highest existing value.

--- src/common/helpers/generate-sku.ts
+++ src/common/helpers/generate-sku.ts
@@ -4,7 +4,13 @@ import { ProductDocument } from 'src/modules/product/schemas/product.model';
 export const generateUniqueSKU = async (
   productModel: Model<ProductDocument>,
 ): Promise<string> => {
-  const productCount = await productModel.countDocuments();
-  const skuNumber = productCount + 1;
+  // const productCount = await productModel.countDocuments();
+  const lastProduct = await productModel
+    .findOne()
+    .sort({ sku: -1 })
+    .select('sku')
+    .lean();
+
+  const skuNumber = parseInt(lastProduct.sku) + 1;
   return skuNumber.toString().padStart(5, '0');
 };
